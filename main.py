730bef3a0b57da0c0a0bcf0a2320ca386171fac8
IBEABUCHI CHINAECHEREM EMMANUEL <105367092+Emmay22k@users.noreply.github.com>
2022-06-01 15:09:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# Check if two words are anagrams 
+# Example:
+#find_anagrams("hello", "check") --> False
+#find_anagrams("below", "elbow") --> True
+
+
+
+
+from readline import write_history_file
+
+
+def find_anagram(word, anagram):
+ # [assignment] Add your code here
+   
+   find_anagram = (wolf, flow)
+
+   str1 = "wolf"
+   str2 = "flow"
+
+    if sorted(str1) = sorted(str2)
+    str = find_anagram
+
+    return True
+
+print(find_anagram)
+  
